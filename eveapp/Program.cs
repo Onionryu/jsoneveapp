4596294ba0b1254495f49446e7a9a09eb09bc408
Joni Toivonoja <Joni_Toivonoja@hotmail.com>
2017-10-25 17:21:31 +0300

    Added documentation

--- eveapp/Program.cs
+++ eveapp/Program.cs
@@ -10,26 +10,23 @@ namespace eveapp
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
-
-                var repositories = ProcessRepositories().Result;
-                foreach (var repo in repositories)
-                {
-                    Console.WriteLine(repo.CorpId);
-                    Console.WriteLine(repo.BDay);
-                    Console.WriteLine(repo.Name);
-                    Console.WriteLine(repo.Gender);
-                    Console.WriteLine(repo.Race);
-                    Console.WriteLine(repo.Bloodline);
-                    Console.WriteLine(repo.Description);
-                    Console.WriteLine(repo.Alliance);
-                    Console.WriteLine(repo.Ancestry);
-                    Console.WriteLine(repo.Security);
-                    Console.WriteLine();
-                }
+            var repositories = ProcessRepositories().Result;
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.CorpId);
+                Console.WriteLine(repo.BDay);
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Gender);
+                Console.WriteLine(repo.Race);
+                Console.WriteLine(repo.Bloodline);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.Alliance);
+                Console.WriteLine(repo.Ancestry);
+                Console.WriteLine(repo.Security);
+                Console.WriteLine();
+            }
             
             Console.ReadLine();
         }
@@ -37,23 +34,23 @@ namespace eveapp
         private static async Task<List<Repository>> ProcessRepositories()
         {
 
-                var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
-                var client = new HttpClient();
-                //client.DefaultRequestHeaders.Accept.Clear();
-                //client.DefaultRequestHeaders.Accept.Add(
-                //    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-                //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            //client.DefaultRequestHeaders.Add("X-User-Agent", "");
 
-                var streamTask = client.GetStreamAsync("https://esi.tech.ccp.is/latest/characters/97138401/?datasource=tranquility");
-                var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
-                return repositories;
-                //var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
-                //var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
-                //var msg = await stringTask;
-                //Console.Write(msg);
+
+            //documentation of the API is in https://esi.tech.ccp.is/latest/#!/Character/get_characters_character_id
+            var streamTask = client.GetStreamAsync("https://esi.tech.ccp.is/latest/characters/97138401/?datasource=tranquility");
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+            return repositories;
+            //var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+            //var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            //var msg = await stringTask;    
+            //Console.Write(msg);
 
         }
-
-
     }
 }
