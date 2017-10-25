e18e947302d87db6c4b1581f8faf0384447dc608
Joni Toivonoja <Joni_Toivonoja@hotmail.com>
2017-10-25 16:56:04 +0300

    Add project files.

---
+++ eveapp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace eveapp
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+                var repositories = ProcessRepositories().Result;
+                foreach (var repo in repositories)
+                {
+                    Console.WriteLine(repo.CorpId);
+                    Console.WriteLine(repo.BDay);
+                    Console.WriteLine(repo.Name);
+                    Console.WriteLine(repo.Gender);
+                    Console.WriteLine(repo.Race);
+                    Console.WriteLine(repo.Bloodline);
+                    Console.WriteLine(repo.Description);
+                    Console.WriteLine(repo.Alliance);
+                    Console.WriteLine(repo.Ancestry);
+                    Console.WriteLine(repo.Security);
+                    Console.WriteLine();
+                }
+            
+            Console.ReadLine();
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+
+                var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+                var client = new HttpClient();
+                //client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(
+                //    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+                var streamTask = client.GetStreamAsync("https://esi.tech.ccp.is/latest/characters/97138401/?datasource=tranquility");
+                var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+                return repositories;
+                //var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+                //var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+                //var msg = await stringTask;
+                //Console.Write(msg);
+
+        }
+
+
+    }
+}
---
+++ eveapp/Repository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace eveapp
+{
+    [DataContract(Name="repo")]
+    public class Repository
+    {
+
+        [DataMember(Name = "corporation_id")]
+        public string CorpId { get; set; }
+
+        [DataMember(Name = "birthday")]
+        public string BDay { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "gender")]
+        public string Gender { get; set; }
+
+        [DataMember(Name = "race_id")]
+        public string Race { get; set; }
+
+        [DataMember(Name = "bloodline_id")]
+        public string Bloodline { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "alliance_id")]
+        public string Alliance { get; set; }
+
+        [DataMember(Name = "ancestry_id")]
+        public string Ancestry { get; set; }
+
+        [DataMember(Name = "security_status")]
+        public string Security { get; set; }
+
+        /*[DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name = "homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name = "watchers")]
+        public int Watchers { get; set; }
+
+        [DataMember(Name = "pushed_at")]
+        private string JsonDate { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime LastPush
+        {
+            get
+            {
+                return DateTime.ParseExact(JsonDate, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            }
+        }
+        */
+
+    }
+}
